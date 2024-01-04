7820a1206f420990462ddf61c86ec524c98ab8af
WAKD Gunasekara <137909922+kavinduGunasekara@users.noreply.github.com>
2024-01-04 23:51:04 +0530

    first commit 2024.01.04
    create Database and connect it

---
+++ user.php
@@ -0,0 +1,66 @@
+<?php 
+include 'connect.php';
+if(isset($_POST['submit'])){
+    $name=$_POST[''];
+    $email=$_POST[''];
+    $mobile=$_POST[''];
+    $password=$_POST[''];
+}
+
+?>
+
+
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <div class="container my-5">
+        <form method="post">
+            <div class="form-group">
+                <label>Name</label>
+                <input type="email" class="form-control" 
+                placeholder="Enter your name"
+                 name="name" autocomplete="off">
+</div>
+<div class="form-group">
+                <label>Email</label>
+                <input type="email" class="form-control" 
+                placeholder="Enter your email"
+                 name="email" autocomplete="off">
+</div>
+<div class="form-group">
+                <label>Mobile</label>
+                <input type="email" class="form-control" 
+                placeholder="Enter your mobile number"
+                 name="mobile" autocomplete="off">
+</div>
+<div class="form-group">
+                <label>Password</label>
+                <input type="email" class="form-control" 
+                placeholder="Enter your password"
+                 name="password" autocomplete="off">
+</div>
+
+
+                <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+        </form>
+    </div>
+
+
+
+</body>
+
+</html>
