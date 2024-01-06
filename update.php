0cddd70a4e5fc280f5b60bcb897698e2ea267004
WAKD Gunasekara <137909922+kavinduGunasekara@users.noreply.github.com>
2024-01-06 23:14:05 +0530

    complete project with CRUD operation
    2024.01.06

--- update.php
+++ update.php
@@ -1,6 +1,15 @@
 <?php 
 include 'connect.php';
 $id=$_GET['updateid'];
+$sql="select * from `crud` where id=$id";
+$result=mysqli_query($con,$sql);
+$row=mysqli_fetch_assoc($result);
+$name=$row['name'];
+$email=$row['email'];
+$mobile=$row['mobile'];
+$password=$row['password'];
+
+
 if(isset($_POST['submit'])){
     $name=$_POST['name'];
     $email=$_POST['email'];
@@ -12,9 +21,9 @@ if(isset($_POST['submit'])){
     password='$password'WHERE id=$id";
     $result=mysqli_query($con,$sql);
     if($result){
-       echo "Data updatesuccessfully";
+      // echo "Data updatesuccessfully";
 
-      // header('location:display.php');
+       header('location:display.php');
     }else{
         die(mysqli_error($con));
 
@@ -47,26 +56,32 @@ if(isset($_POST['submit'])){
                 <label>Name</label>
                 <input type="text" class="form-control" 
                 placeholder="Enter your name"
-                 name="name" autocomplete="off">
+                 name="name" autocomplete="off" value=<?php
+                 echo $name;?>
+                 >
 </div>
 <div class="form-group">
                 <label>Email</label>
                 <input type="email" class="form-control" 
                 placeholder="Enter your email"
-                 name="email" autocomplete="off">
+                 name="email" autocomplete="off" value=<?php
+                 echo $email;?>>
 </div>
 <div class="form-group">
                 <label>Mobile</label>
                 <input type="text" class="form-control" 
                 placeholder="Enter your mobile number"
-                 name="mobile" autocomplete="off">
+                 name="mobile" autocomplete="off"  value=<?php
+                 echo $mobile;?>>
 </div>
 <div class="form-group">
                 <label>Password</label>
                 <input type="text" class="form-control" 
                 placeholder="Enter your password"
-                 name="password" >
+                 name="password"  value=<?php
+                 echo $password;?>>
 </div>
+
 
 
                 <button type="submit" 
